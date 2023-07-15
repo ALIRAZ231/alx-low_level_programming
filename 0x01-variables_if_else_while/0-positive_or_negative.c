ceb58b0cc9c56f5fd43f3239870f626ca28614b6
Ali <arazgui3@gmail.com>
2023-07-14 17:25:40 -0700

    Coding

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,12 +3,12 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
+*main - Entry point
 *
-* Description: Generate a random number and print whether it is 
-* positive, negative, or zero.
+*Description: Generate a random number and print whether it is 
+*positive, negative, or zero.
 *
-* Return: Always 0 (Success)
+*Return: Always 0 (Success)
 */
 int main(void)
 {
